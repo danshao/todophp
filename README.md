7084c168ca0316d562bee960f35ec0b25533cda4
Dan Shao <danshao@gmail.com>
2015-08-24 01:23:39 +0800

    updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# todophp
+# To Do List (PHP/MySql/jQuery)
+
+Features
+--------
+1. Add tasks
+2. Check off tasks and move to a completed area
+3. Ability to update status of a given task
+4. Edit task
+5. Rearrange incomplete tasks
+
+
+Instructions
+------------
+1. Create table with ```todo.sql```
+2. Fill out database info in ```connect.php```
--- connect.php
+++ connect.php
@@ -1,12 +1,12 @@
 <?php
 
 	// Set DB variables here
-	$db_host		= '0.0.0.0';
-	$db_user		= 'root';
-	$db_pass		= 'root';
-	$db_database	= 'todo'; 
+	$db_host		= '';
+	$db_user		= '';
+	$db_pass		= '';
+	$db_database	= ''; 
 
-	$link = @mysql_connect($db_host,$db_user,$db_pass) or die('Unable to establish a DB connection');
+	$link = @mysql_connect($db_host, $db_user, $db_pass) or die('Unable to establish a DB connection');
 
 	mysql_set_charset('utf8');
 	mysql_select_db($db_database,$link);
